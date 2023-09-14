9ce38fb34efb2043880c1abb801d910a5dd0cc3b
sctwomey <sctwomey@gmail.com>
2023-09-14 12:37:13 -0700

    Add questions array to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,57 @@ let quizTimerEl = document.querySelector("#quiz-timer");
 // Sets the questions for the quiz from the question and answers array.
 let randomQuestionsArray = [];
 let questionsIndex = 0;
+
+let quizQuestions = [
+    {
+        question: 'Inside which HTML element do we put the JavaScript file?',
+        answer: '<script>',
+        userSelections: [{ userSelection: '<js>' }, { userSelection: '<script>' }, { userSelection: '<javascript>' }, { userSelection: '<scripting>' }]
+    },
+    {
+        question: 'How do you write an "if" statement in JavaScript?',
+        answer: 'if (i == 5)',
+        userSelections: [{ userSelection: 'if (i == 5)' }, { userSelection: 'if i = 5' }, { userSelection: 'if i == 5 then' }, { userSelection: 'if i = 5' }]
+    },
+    {
+        question: 'What is the correct way to write a JavaScript array?',
+        answer: 'let colors = ["red", "green", "blue"];',
+        userSelections: [{ userSelection: 'let colors = ("red" - "green" - "blue");' }, { userSelection: 'let colors = "red",  "green",  "blue";' }, { userSelection: 'let colors = ["red", "green", "blue"];' }, { userSelection: 'let colors = 1("red"), 2("green"), 3("blue");' }]
+    },
+    {
+        question: 'How do you round the number 7.25, to the nearest integer (whole number)?',
+        answer: 'Math.round(7.25)',
+        userSelections: [{ userSelection: 'Math.rnd(7.25)' }, { userSelection: 'rnd(7.25)' }, { userSelection: 'round(7.25)' }, { userSelection: 'Math.round(7.25)' }]
+    },
+    {
+        question: 'How do you find the number with the highest value of x and y?',
+        answer: 'Math.max(x, y)',
+        userSelections: [{ userSelection: 'top(x, y)' }, { userSelection: 'Math.max(x, y)' }, { userSelection: 'Math.ceil(x, y)' }, { userSelection: 'ceil(x, y)' }]
+    },
+    {
+        question: 'JavaScript is the same as Java.',
+        answer: 'False',
+        userSelections: [{ userSelection: 'True' }, { userSelection: 'False' }]
+    },
+    {
+        question: 'Which event occurs when the user "clicks" on an HTML element?',
+        answer: 'onclick',
+        userSelections: [{ userSelection: 'onmouseclick' }, { userSelection: 'onmouseover' }, { userSelection: 'onclick' }, { userSelection: 'onchange' }]
+    },
+    {
+        question: 'How do you write "Hello World" in an alert box?',
+        answer: 'alert("Hello World");',
+        userSelections: [{ userSelection: 'alert("Hello World");' }, { userSelection: 'msg("Hello World");' }, { userSelection: 'msgBox("Hello World");' }, { userSelection: 'alertBox("Hello World");' }]
+    },
+    {
+        question: 'The external JavaScript file must contain the <script> tag.',
+        answer: 'False',
+        userSelections: [{ userSelection: 'True' }, { userSelection: 'False' }]
+    },
+    {
+        question: 'What is the correct syntax for referring to an external script called "xxx.js"?',
+        answer: '<script src="xxx.js">',
+        userSelections: [{ userSelection: '<script href="xxx.js">' }, { userSelection: '<script name="xxx.js">' }, { userSelection: '<script ref="xxx.js">' }, { userSelection: '<script src="xxx.js">' }]
+    }
+];
+
